2a7f9f6b393b0db9afbeb4619bbb46e29eb9edfb
David De Anda <omarandstuff@gmail.com>
2017-04-20 23:21:16 -0500

    Open confirmation service for users

---
+++ app/services/users/open_confirmation.rb
@@ -0,0 +1,17 @@
+module Users
+  class OpenConfirmation
+    extend ProcessWrapper
+
+    attr_reader :user
+
+    def initialize(user:)
+      @user = user
+    end
+
+    def process
+      user if user.update confirmation_token: Utils::GenerateToken.for(data: { user_id: user.id, created_at: Time.now }),
+                          confirmation_sent_at: Time.now,
+                          confirmed_at: nil
+    end
+  end
+end
---
+++ spec/services/users/open_confirmation_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe Users::OpenConfirmation do
+  describe '#process' do
+    context 'when a new user object is present' do
+      it 'sets the user confirmetion params and returns the user' do
+        user = FactoryGirl.create :user
+        service = Users::OpenConfirmation.new user: user
+
+        result = service.process
+
+        expect(result).to eq(user)
+        expect(user.confirmation_token).to_not be_nil
+        expect(user.confirmation_sent_at).to_not be_nil
+        expect(user.confirmed_at).to be_nil
+      end
+    end
+
+    context 'when a confirmed user object is present' do
+      it 'sets the user confirmetion params, unconfirm and returns the user' do
+        user = FactoryGirl.create :user, confirmed_at: Time.now
+        service = Users::OpenConfirmation.new user: user
+
+        result = service.process
+
+        expect(result).to eq(user)
+        expect(user.confirmation_token).to_not be_nil
+        expect(user.confirmation_sent_at).to_not be_nil
+        expect(user.confirmed_at).to be_nil
+      end
+    end
+  end
+end
