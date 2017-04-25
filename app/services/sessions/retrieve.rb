a3fa1696da1c29b861883a96ebcead10fe3b6538
David De Anda <omarandstuff@gmail.com>
2017-04-24 20:16:32 -0500

    Just return user as responsibility

--- app/services/sessions/retrieve.rb
+++ app/services/sessions/retrieve.rb
@@ -10,11 +10,9 @@ module Sessions
 
     def process
       data = Utils::DeserializeToken.for token: token
-      if data and Time.at(data['created_at']) + Sessions.configuration.session_token_time_alive >= Time.now
-        user =  User.find_by_id data['user_id']
-        new_token = Utils::GenerateToken.for data: { user_id: user.id, created_at: Time.now }
 
-        return { user: user, token: new_token }
+      if data and (Time.at(data['created_at']) + Sessions.configuration.session_token_time_alive) >= Time.now
+        User.find_by_id data['user_id']
       end
     end
   end
--- spec/services/sessions/retrieve_spec.rb
+++ spec/services/sessions/retrieve_spec.rb
@@ -3,15 +3,24 @@ require 'rails_helper'
 describe Sessions::Retrieve do
   describe '#process' do
     context 'when the right token is present' do
-      it 'returs a user and a new token' do
-        user = FactoryGirl.create :user
+      it 'returs the autheticated' do
+        user = FactoryGirl.create :user, :confirmed
         token = Sessions::Create.for credential: user.username, password: user.password
 
         service = Sessions::Retrieve.new token: token
-        result = service.process
 
-        expect(result[:user]).to eq(user)
-        expect(result[:token]).to be_kind_of(String)
+        expect(service.process).to eq(user)
+      end
+
+      context 'but the token alive time had passed' do
+        it 'returs nil' do
+          user = FactoryGirl.create :user, :confirmed
+          token = Utils::GenerateToken.for data: { user_id: user.id, created_at: (Time.now - Sessions.configuration.session_token_time_alive).to_i }
+
+          service = Sessions::Retrieve.new token: token
+
+          expect(service.process).to be_nil
+        end
       end
     end
 
