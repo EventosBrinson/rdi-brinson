060b2c69ebb199a881746edba31b9e9b25eb0960
David De Anda <omarandstuff@gmail.com>
2017-05-02 21:20:21 -0500

    Retrieve if user confirmed

--- app/services/sessions/retrieve.rb
+++ app/services/sessions/retrieve.rb
@@ -12,7 +12,8 @@ module Sessions
       data = Utils::DeserializeToken.for token: token
 
       if data and (Time.at(data['created_at']) + Sessions.configuration.session_token_time_alive) >= Time.now
-        User.find_by_id data['user_id']
+        user = User.find_by_id data['user_id']
+        user if user and user.confirmed?
       end
     end
   end
