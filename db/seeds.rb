af55790cd48d892f11f247d9c58c07f98ef850f4
David De Anda <omarandstuff@gmail.com>
2017-05-02 21:05:41 -0500

    Seed super users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-User.create firstname: 'David', lastname: 'De Anda', email: 'omarandstuff@gmail.com', username: "omarandstuff", role: User::ROLES[1], password: '12345678'
+omar = User.create firstname: 'David', lastname: 'De Anda', email: 'omarandstuff@gmail.com', username: "omarandstuff", role: User::ROLES[1], password: '12345678'
+javier = User.create firstname: 'Javier Ivan', lastname: 'Martinez Alvarez', email: 'javier_iv_@hotmail.com', username: "JavierXbolt", role: User::ROLES[0], password: '12345678'
+Users::OpenConfirmation.for user: omar
+Users::OpenConfirmation.for user: javier
+UserMailer.invitation_mail(omar).deliver_now
+UserMailer.invitation_mail(javier).deliver_now
