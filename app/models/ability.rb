36ef3bac99ac76e71895b772237112abdcceffe7
David De Anda <omarandstuff@gmail.com>
2017-05-09 22:04:28 -0500

    Main user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,10 @@ class Ability
     if user.admin? || user.staff?
       can :manage, User
       cannot :update, User do |subject|
-        (subject.id == user.id and subject.role_changed?) or (high_level_user?(subject) and subject.id != user.id)
+        ((subject.id == user.id and subject.role_changed?) or (high_level_user?(subject) and subject.id != user.id)) and !user.main
+      end
+      cannot :create, User do |subject|
+        high_level_user?(subject) and !user.main
       end
     end
 
@@ -19,13 +22,15 @@ class Ability
       can :update, User do |subject|
         subject.id == user.id and !subject.role_changed?
       end
+
+      cannot :index, User
     end
   end
 
   private
 
   def high_level_user?(user)
-    if user.role_changed?
+    if user.role_changed? and !user.new_record?
       user.role_was == :admin || user.role_was == :staff
     else
       user.admin? || user.staff?
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -10,16 +10,36 @@ RSpec.describe Ability do
 
   subject { Ability.new(user) }
 
+  context 'when the user is a main user' do
+    let(:user) { FactoryGirl.create(:user, :admin, :main) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    let(:other_user_role_changed) { other_user.role = :admin; other_user }
+    let(:admin_user_ready_to_create) { FactoryGirl.build :user, :admin }
+    let(:admin_role_changed) { admin.role == :user; admin }
+
+    it{ should be_able_to(:manage, User) }
+    it{ should be_able_to(:manage, user) }
+    it{ should be_able_to(:manage, other_user) }
+    it{ should be_able_to(:update, other_user_role_changed) }
+    it{ should be_able_to(:update, user_role_changed) }
+    it{ should be_able_to(:create, admin_user_ready_to_create) }
+    it{ should be_able_to(:update, admin) }
+    it{ should be_able_to(:update, admin_role_changed) }
+    it{ should be_able_to(:update, staff) }
+  end
+
   context 'when the user is an admin or staff' do
     let(:user) { FactoryGirl.create(:user, :admin) }
     let(:other_user) { FactoryGirl.create(:user) }
     let(:other_user_role_changed) { other_user.role = :admin; other_user }
+    let(:admin_user_ready_to_create) { FactoryGirl.build :user, :admin }
 
     it{ should be_able_to(:manage, User) }
     it{ should be_able_to(:manage, user) }
     it{ should be_able_to(:manage, other_user) }
     it{ should be_able_to(:update, other_user_role_changed) }
     it{ should_not be_able_to(:update, user_role_changed) }
+    it{ should_not be_able_to(:create, admin_user_ready_to_create) }
     it{ should_not be_able_to(:update, admin) }
     it{ should_not be_able_to(:update, staff) }
   end
@@ -33,5 +53,6 @@ RSpec.describe Ability do
     it{ should be_able_to(:update, user) }
     it{ should_not be_able_to(:update, user_role_changed) }
     it{ should_not be_able_to(:manage, other_user) }
+    it{ should_not be_able_to(:index, User) }
   end
 end
