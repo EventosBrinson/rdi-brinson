bf3e3b49a09e8a4f24a2ab63163bdd516248ca50
David De Anda <omarandstuff@gmail.com>
2017-05-11 00:41:09 -0500

    Application controller handle cancan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::API
+  include CanCan::ControllerAdditions
   attr_reader :current_user, :new_token
+
+  rescue_from CanCan::AccessDenied, with: :access_denied
 
   private
 
   def authenticate_request
     @current_user = Sessions::Retrieve.for token: request.headers[:HTTP_AUTH_TOKEN]
     head :forbidden unless @current_user
+  end
+
+  def access_denied
+    head :forbidden
   end
 end
