0c2d6fd86462932ae877ed6c45af0eae3ba8e707
David De Anda <omarandstuff@gmail.com>
2017-05-29 22:34:19 -0500

    Document test refactor

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe Document, type: :model do
   it { should have_db_column(:title).of_type(:string).with_options(null: false) }
 
   it { should belong_to(:client) }
+  it { should have_attached_file(:file) }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :title }
   it { should validate_presence_of :client }
-
-  it { should have_attached_file(:file) }
   it { should validate_attachment_presence(:file) }
   it { should validate_attachment_content_type(:file)
               .allowing('image/jpeg', 'application/pdf')
