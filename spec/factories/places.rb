c6185e8e18a21ab1304e24de954b69ac6eaf50d5
David De Anda <omarandstuff@gmail.com>
2017-06-03 20:47:43 -0500

    Place factory

---
+++ spec/factories/places.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :place do
+    name { Faker::StarWars.planet }
+    address_line_1 { Faker::Address.street_name + ' #' + Faker::Address.building_number }
+    address_line_2 { Faker::Address.city + ', CP ' + Faker::Address.zip_code }
+    latitude { Faker::Address.latitude }
+    longitude { Faker::Address.longitude }
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
