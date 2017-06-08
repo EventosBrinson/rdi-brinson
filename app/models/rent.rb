a1b38bce7e95b01d8b8c6593ef1bd3c481dee331
David De Anda <omarandstuff@gmail.com>
2017-06-07 22:44:31 -0500

    Client model

---
+++ app/models/rent.rb
@@ -0,0 +1,19 @@
+class Rent < ApplicationRecord
+  STATUSES = [:reserved, :on_route, :delivered, :on_pick_up, :pending, :finalized]
+
+  belongs_to :client
+  belongs_to :place
+
+  validates :delivery_time, presence: true
+  validates :pick_up_time, presence: true
+  validates :product, presence: true
+  validates :price, presence: true
+  validates_numericality_of :price, greater_than_or_equal_to: 0
+  validates_numericality_of :discount, greater_than_or_equal_to: 0
+  validates_numericality_of :additional_charges, greater_than_or_equal_to: 0
+  validates :rent_type, presence: true
+  validates :status, presence: true
+
+  enum rent_type: Client::RENT_TYPES
+  enum status: STATUSES
+end
