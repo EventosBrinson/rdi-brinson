425d768b4140088f242d129d443a0cee6d57633d
David De Anda <omarandstuff@gmail.com>
2017-06-11 16:35:49 -0500

    Ability refactor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,7 @@ class Ability
     user ||= User.new
 
     if user.admin? || user.staff?
-      can :manage, User
-      cannot :update, User do |subject|
-        ((subject.id == user.id and subject.role_changed?) or (high_level_user?(subject) and subject.id != user.id) or to_high_level_user?(subject)) and !user.main
-      end
-      cannot :create, User do |subject|
-        high_level_user?(subject) and !user.main
-      end
+      admin_on_users(user)
       can :manage, Client
       can :manage, Document
       can :manage, Place
@@ -19,36 +13,11 @@ class Ability
     end
 
     if user.user?
-      cannot :index, User
-      can :show, User do |subject|
-        subject.id == user.id
-      end
-      can :update, User do |subject|
-        subject.id == user.id and !subject.role_changed?
-      end
-
-      can :index, Client
-      can :create, Client
-      can [:show, :update], Client do |subject|
-        subject.creator_id == user.id and !subject.active_changed?
-      end
-
-      can [:show, :create, :update, :delete], Document do |subject|
-        subject.client.creator_id == user.id
-      end
-
-      can :index, Place
-      can [:show, :create], Place do |subject|
-        subject.client.creator_id == user.id
-      end
-      can :update, Place do |subject|
-        subject.client.creator_id == user.id and !subject.active_changed?
-      end
-
-      can :index, Rent
-      can [:show, :create, :update], Rent do |subject|
-        subject.creator_id == user.id and subject.client.creator_id == user.id and subject.place.client.creator_id == user.id
-      end
+      user_on_users(user)
+      user_on_clients(user)
+      user_on_documents(user)
+      user_on_places(user)
+      user_on_rents(user)
     end
   end
 
@@ -65,6 +34,56 @@ class Ability
   def to_high_level_user?(user)
     if user.role_changed?
       user.admin? || user.staff?
+    end
+  end
+
+  def admin_on_users(user)
+    can :manage, User
+    cannot :update, User do |subject|
+      ((subject.id == user.id and subject.role_changed?) or (high_level_user?(subject) and subject.id != user.id) or to_high_level_user?(subject)) and !user.main
+    end
+    cannot :create, User do |subject|
+      high_level_user?(subject) and !user.main
+    end
+  end
+
+  def user_on_users(user)
+    can :show, User do |subject|
+      subject.id == user.id
+    end
+    can :update, User do |subject|
+      subject.id == user.id and !subject.role_changed?
+    end
+  end
+
+  def user_on_clients(user)
+    can :index, Client
+    can :create, Client
+    can [:show, :update], Client do |subject|
+      subject.creator_id == user.id and !subject.active_changed?
+    end
+  end
+
+  def user_on_documents(user)
+    can [:show, :create, :update, :delete], Document do |subject|
+      subject.client.creator_id == user.id
+    end
+  end
+
+  def user_on_places(user)
+    can :index, Place
+    can [:show, :create], Place do |subject|
+      subject.client.creator_id == user.id
+    end
+    can :update, Place do |subject|
+      subject.client.creator_id == user.id and !subject.active_changed?
+    end
+  end
+
+  def user_on_rents(user)
+    can :index, Rent
+    can [:show, :create, :update], Rent do |subject|
+      subject.creator_id == user.id and subject.client.creator_id == user.id and subject.place.client.creator_id == user.id
     end
   end
 end
