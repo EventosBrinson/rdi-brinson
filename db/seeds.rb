b85c4164b4d7607812815c0005467dd57602a9fa
David De Anda <omarandstuff@gmail.com>
2017-06-25 17:50:24 -0500

    Not seed Mr ivan

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 omar = User.create firstname: 'David', lastname: 'De Anda', email: 'omarandstuff@gmail.com', username: "omarandstuff", role: :staff, main: true
-javier = User.create firstname: 'Javier Ivan', lastname: 'Martinez Alvarez', email: 'javier_iv_@hotmail.com', username: "JavierXbolt", role: :admin, main: true
+#javier = User.create firstname: 'Javier Ivan', lastname: 'Martinez Alvarez', email: 'javier_iv_@hotmail.com', username: "JavierXbolt", role: :admin, main: true
 Users::OpenConfirmation.for user: omar
-Users::OpenConfirmation.for user: javier
+#Users::OpenConfirmation.for user: javier
 UserMailer.invitation_mail(omar).deliver_now
-UserMailer.invitation_mail(javier).deliver_now
+#UserMailer.invitation_mail(javier).deliver_now
