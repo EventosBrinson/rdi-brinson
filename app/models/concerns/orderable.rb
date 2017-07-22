e037798b185100aaefa47cc7fa853b08f37b9dfe
David De Anda <omarandstuff@gmail.com>
2017-07-22 11:42:42 -0500

    Ordered lower only text

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -7,10 +7,21 @@ module Orderable
       columns.each do |column, order|
         if self.column_names.include?(column.to_s.downcase) and ['asc', 'desc'].include?(order.to_s.downcase)
           orders += ',' unless orders.empty?
-          orders += " LOWER(\"#{ self.name.downcase.pluralize }\".\"#{column.to_s.downcase}\") #{order.to_s.downcase}"
+          case self.column_for_attribute(column.to_s.downcase).type
+          when :string, :text
+            orders += " LOWER(\"#{ table_name }\".\"#{ column.to_s.downcase }\") #{ order.to_s.upcase }"
+          else
+            orders += " \"#{ table_name }\".\"#{ column.to_s.downcase }\" #{ order.to_s.upcase }"
+          end
         end
       end
       orders.empty? ? self.where(nil) : self.reorder(orders)
+    end
+
+    private
+
+    def table_name
+      self.name.downcase.pluralize
     end
   end
 end
