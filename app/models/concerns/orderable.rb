3312ddb5fe922c09227cc54e4db7cc46ecd06094
David De Anda <omarandstuff@gmail.com>
2017-07-22 14:00:04 -0500

    Return table name to inline

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -9,19 +9,13 @@ module Orderable
           orders += ',' unless orders.empty?
           case self.column_for_attribute(column.to_s.downcase).type
           when :string, :text
-            orders += " LOWER(\"#{ table_name }\".\"#{ column.to_s.downcase }\") #{ order.to_s.upcase }"
+            orders += " LOWER(\"#{ self.name.downcase.pluralize }\".\"#{ column.to_s.downcase }\") #{ order.to_s.upcase }"
           else
-            orders += " \"#{ table_name }\".\"#{ column.to_s.downcase }\" #{ order.to_s.upcase }"
+            orders += " \"#{ self.name.downcase.pluralize }\".\"#{ column.to_s.downcase }\" #{ order.to_s.upcase }"
           end
         end
       end
       orders.empty? ? self.where(nil) : self.reorder(orders)
-    end
-
-    private
-
-    def table_name
-      self.name.downcase.pluralize
     end
   end
 end
