0f4b073631d9e00dbaac294a019e42f6161a8717
David De Anda <omarandstuff@gmail.com>
2017-07-22 12:17:31 -0500

    Filter just by permitted methods
    }

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -1,11 +1,15 @@
 module Filterable
   extend ActiveSupport::Concern
 
+  PERMITED_MESSAGES = [:filter, :ordered, :paginated]
+
   module ClassMethods
-    def filter(filtering_params)
+    def filter(filtering_params, permited = nil)
       results = self.where(nil)
-      filtering_params.each do |key, value|
-        results = results.public_send(key, value) if value.present?
+      filtering_params.each do |method, params|
+        if method.present? and self.respond_to?(method.to_sym) and (permited || PERMITED_MESSAGES).include?(method)
+          results = results.public_send(method, params)
+        end
       end
       results
     end
