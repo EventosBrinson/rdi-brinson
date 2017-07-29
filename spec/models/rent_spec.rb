d5bac9331441fec7fefda5c0325a531d73f387b3
David De Anda <omarandstuff@gmail.com>
2017-07-28 19:44:12 -0500

    Specify pick up time too

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -291,13 +291,13 @@ RSpec.describe Rent, type: :model do
 
   describe '.filter_by_time' do
     it 'returns all the rents that are between the times given' do
-      rent_match1 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'de anda', delivery_time: 1.day.ago
-      rent_match2 = FactoryGirl.create :rent, product: 'DAVID', additional_charges_notes: 'gomez', delivery_time: 1.day.ago
-      rent_match3 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'segoviano', delivery_time: Time.now
-      rent_match4 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'zan', delivery_time: Time.now
-      rent_not_match = FactoryGirl.create :rent, product: 'Roberto', additional_charges_notes: 'Bolaños', delivery_time: 3.days.from_now
+      rent_match1 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'de anda', delivery_time: 1.day.ago, pick_up_time: 1.day.ago
+      rent_match2 = FactoryGirl.create :rent, product: 'DAVID', additional_charges_notes: 'gomez', delivery_time: 1.day.ago, pick_up_time: 1.day.ago
+      rent_match3 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'segoviano', delivery_time: Date.today, pick_up_time: 1.day.ago
+      rent_match4 = FactoryGirl.create :rent, product: 'david', additional_charges_notes: 'zan', delivery_time: Date.today, pick_up_time: 1.day.ago
+      rent_not_match = FactoryGirl.create :rent, product: 'Roberto', additional_charges_notes: 'Bolaños', delivery_time: 3.days.from_now, pick_up_time: 3.day.from_now
 
-      rents_filtered_by_time = Rent.filter_by_time(1.day.ago.beginning_of_day, Time.now.end_of_day, :delivery_time, :pick_up_time)
+      rents_filtered_by_time = Rent.filter_by_time(1.day.ago.beginning_of_day, Date.today.end_of_day, :delivery_time, :pick_up_time)
 
       expect(rents_filtered_by_time.size).to eq(4)
       expect(rents_filtered_by_time.first).to eq(rent_match1)
