b8404adab6bdcfce5849c090cb16c2fdba0acb98
David De Anda <omarandstuff@gmail.com>
2017-07-31 00:11:00 -0500

    Fix month numbers

--- app/helpers/formatted_time_helper.rb
+++ app/helpers/formatted_time_helper.rb
@@ -1,6 +1,6 @@
 module FormattedTimeHelper
   DAY_NAMES = ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
-  MONTH_NAMES = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+  MONTH_NAMES = ['', 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
 
   def week_day_name(date)
     DAY_NAMES[date.wday].capitalize
